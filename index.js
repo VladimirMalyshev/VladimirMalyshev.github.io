5e76b0d6510c7566d4a67a3b3cd3fb050deb787e
Vladimir Malyshev <malyshev200522@gmail.com>
2025-02-27 22:06:45 +0300

    fix download pdf

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-var element = document.getElementById('resume').cloneNode(true);
-var button = element.querySelector('.download-btn');
-if (button) {
-    button.remove();
-}
 function downloadPDF() {
-  var element = document.getElementById('resume');
-  var opt = {
-    margin:       0.5,
-    filename:     'Vladimir_Malyshev_Resume.pdf',
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2 },
-    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-  };
-  html2pdf().from(element).set(opt).save();
+  const btn = document.querySelector('.download-btn');
+  btn.style.display = 'none';
+
+  window.print();
+
+  btn.style.display = 'block';
 }
+  
